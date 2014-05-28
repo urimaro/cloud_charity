bcdfb64faeea28c9ef88c94fdb95e541290b7a6f
satomicchy <satomi_t_sora@yahoo.co.jp>
2014-05-29 00:27:52 +0900

    implement twitter oauth

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,3 +1,7 @@
 class Identity < ActiveRecord::Base
   belongs_to :user
+
+  def self.find_for_oauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, :omniauth_providers => [:twitter, :facebook]
+
+  def self.find_for_oauth(auth)
+    identity      = Identity.find_for_oauth(auth)
+    user          = (identity.user || identity.build_user)
+    user.email    = (auth.info.email || "#{auth.uid}@example.com")
+    user.password = Devise.friendly_token[0,20]
+    user.name     = auth.info.name
+    user.save!
+    identity.update(user: user) unless identity.user_id
+    user
+  end
 end
