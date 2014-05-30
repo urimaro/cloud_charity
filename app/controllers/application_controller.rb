40161944aaab4c34dccb6932e0dbe709630ef6d8
satomicchy <satomi_t_sora@yahoo.co.jp>
2014-05-30 21:01:55 +0900

    implement sample twitter bootstrap top page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  layout :layout
+
+  private
+  def layout
+    !devise_controller? && "application"
+  end
 end
